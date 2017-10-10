5298148cfbfe924ffc276f1b0e0c04a49cb67965
22saxe22 <22saxe22@gmail.com>
2017-10-10 15:51:04 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main
+{
+ 
+    /**
+     * Constructor for objects of class main
+     */
+    public main()
+    {
+        System.out.println("Hello World");
+    }
+
+}
